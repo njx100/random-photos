f199c56857f619d2304363d61b07bb362ceaca69
njx100 <dnnguyen100@gmail.com>
2023-04-05 16:46:57 +0700

    completed all basic functions

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const btnEl = document.querySelector(".btn");
+const imageContainerEl = document.querySelector(".image-container");
 
+function addNewImages() {
+    const newImgEl = document.createElement("img");
+    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`;
+    imageContainerEl.appendChild(newImgEl);
+};
+
+function addSixImages() {
+for (let index = 0; index < 6; index++) {
+    addNewImages();  
+};
+};
+addSixImages();
+
+btnEl.addEventListener("click", addSixImages);
